fdd7b411629fd9bc6fe8defba9e6c94aaa553d99
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-07-24 17:22:47 +0300

    commit tree 5aa0f28b802a534c82e66979e504b2b9678473cc

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
-  const [color, setColor] = useState("black");
+  const [color, setColor] = useState("green");
 
   useEffect(() => {
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
@@ -12,6 +12,26 @@ function Ticker() {
     };
   }, []);
 
+  //using the useRef hook, we can be able to persist the previous price
+  const prevPriceRef = useRef(price)
+
+  //write a side effect with the price as the dependency
+  useEffect(() => {
+
+    // use current value of the ref
+    const prevPrice = prevPriceRef.current
+
+    if(price > prevPrice){
+      setColor("green")
+    } else if (price < prevPrice) {
+      setColor("red")
+    } else {
+      setColor("black")
+    }
+
+    // set new value of ref as price --- does not trigger a re-render
+    prevPriceRef.current = price
+  }, [price])
   return (
     <div>
       <h1>TickerMaster</h1>
